a287846e60b3b1ea3bfdab92eedf44b09a00a5e1
ckhilton <ckirkhilton@gmail.com>
2020-06-09 00:09:00 -0600

    Add Basic_HTML_2

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function myFirstFunction() {
+    var A = "Hi instructor! :)";
+    var B = A.fontcolor("red");
+    document.getElementById("red_text").innerHTML = B;
+}
+
+function Function_2() {
+    var statement = "I\'m enjoying this bootcamp. ";
+    statement += "It\'s a lot of fun!";
+    document.getElementById("concatenate").innerHTML = statement;
+}
